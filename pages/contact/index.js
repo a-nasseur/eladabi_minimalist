f5b179ec5fcd8b600cb9a18b09316a1e1e0204f2
abdelhak <a.nasseur.dev@gmail.com>
2022-08-16 16:28:36 +0100

    contact func name changed

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -24,7 +24,7 @@ const validationSchema = Yup.object().shape({
 });
 
 
-const index = () => {
+const Contact = () => {
   const [loading, setLoading] = useState(false)
 
   
@@ -99,4 +99,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Contact;
